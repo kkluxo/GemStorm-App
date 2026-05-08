aa7712fac4703ca96bbaa9503ae25d2efea41ed0
kkluxo <7ft676f24v@privaterelay.appleid.com>
2026-05-08 09:30:09 +0300

    Create checkout.js

---
+++ js/checkout.js
@@ -0,0 +1,11 @@
+let appliedPromo = null;
+let isPromoMode = false;
+let checkoutData = {
+  paymentMethod: "",
+  senderName: "",
+  email: ""
+};
+
+function renderCheckoutForm() { ... }
+function renderPaymentPage() { ... }
+function createOrder() { ... }

125cffd5ab27cbe09ad716a14d50583bc064b14c
kkluxo <7ft676f24v@privaterelay.appleid.com>
2026-05-08 09:31:37 +0300

    Create catalog.js

---
+++ js/catalog.js
@@ -0,0 +1,22 @@
+let currentFilter = "Brawl Stars";
+let searchQuery = "";
+
+function renderCatalog() {
+  // ... рендер каталога
+}
+
+function renderFilterModal() {
+  // ... модалка фильтров
+}
+
+function attachCardEvents() {
+  // ... привязка событий к карточкам
+}
+
+function updateAllCards() {
+  // ... обновление кнопок
+}
+
+function updateSingleCard(productId) {
+  // ... обновление одной карточки
+}

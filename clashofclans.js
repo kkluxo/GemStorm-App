3ea9af4a795cd844364a4031385669034d2251b7
kkluxo <7ft676f24v@privaterelay.appleid.com>
2026-06-27 21:13:18 +0300

    Delete clashofclans.js

--- clashofclans.js
+++
@@ -1,3 +0,0 @@
-window.clashOfClansProducts = [
-  [92, "Пропуск Clash of Clans", "8.99", "8.99", "Пропуск", "Пропуски", 1, [], true, "https://i.postimg.cc/bNrLqjsm/IMG-7691.png"]
-].map(p => mapProduct(p));

f95050ccc24525a85f188236534560408734affa
kkluxo <7ft676f24v@privaterelay.appleid.com>
2026-06-27 17:39:56 +0300

    Update clashofclans.js

--- clashofclans.js
+++ clashofclans.js
@@ -1,1 +1,3 @@
-y
+window.clashOfClansProducts = [
+  [92, "Пропуск Clash of Clans", "8.99", "8.99", "Пропуск", "Пропуски", 1, [], true, "https://i.postimg.cc/bNrLqjsm/IMG-7691.png"]
+].map(p => mapProduct(p));

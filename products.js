feb335278513b2242bc99729c0df26933e4ed649
kkluxo <7ft676f24v@privaterelay.appleid.com>
2026-06-28 15:00:28 +0300

    Update products.js

--- products.js
+++ products.js
@@ -168,12 +168,14 @@ window.hdProducts = makeProducts(hdRaw, 'hd_');
 
 // ===== ROBLOX =====
 const robloxRaw = [
-  [1, "80 робуксов", "0.49", "0.49", "Робуксы", "Робуксы", 1, [], true, "https://i.postimg.cc/Wzy4fm0g/06BADDD1-A06A-4C6F-B498-E73C85183DE5.jpg"],
-  [2, "80 робуксов", "0.99", "0.99", "Робуксы", "Робуксы", 1, [], true, "https://i.postimg.cc/Wzy4fm0g/06BADDD1-A06A-4C6F-B498-E73C85183DE5.jpg"],
-  [3, "400 робуксов", "4.99", "4.99", "Робуксы", "Робуксы", 1, [], true, "https://i.postimg.cc/Wzy4fm0g/06BADDD1-A06A-4C6F-B498-E73C85183DE5.jpg"],
-  [4, "800 робуксов", "9.99", "9.99", "Робуксы", "Робуксы", 1, [], true, "https://i.postimg.cc/Wzy4fm0g/06BADDD1-A06A-4C6F-B498-E73C85183DE5.jpg"],
-  [5, "1200 робуксов", "14.99", "14.99", "Робуксы", "Робуксы", 1, [], true, "https://i.postimg.cc/Wzy4fm0g/06BADDD1-A06A-4C6F-B498-E73C85183DE5.jpg"],
-  [6, "1700 робуксов", "19.99", "19.99", "Робуксы", "Робуксы", 1, [], true, "https://i.postimg.cc/Wzy4fm0g/06BADDD1-A06A-4C6F-B498-E73C85183DE5.jpg"],
+  [1, "80 робуксов", "0.49", "0.49", "Робуксы", "Робуксы", 2, [], true, "https://i.postimg.cc/5yZPJDmx/2845F54E-AE8D-4921-BD2C-4427AC3B4C93.jpg"],
+  [2, "80 робуксов", "0.99", "0.99", "Робуксы", "Робуксы", 2, [], true, "https://i.postimg.cc/5yZPJDmx/2845F54E-AE8D-4921-BD2C-4427AC3B4C93.jpg"],
+  [3, "400 робуксов", "4.99", "4.99", "Робуксы", "Робуксы", 2, [], true, "https://i.postimg.cc/5yZPJDmx/2845F54E-AE8D-4921-BD2C-4427AC3B4C93.jpg"],
+  [4, "800 робуксов", "9.99", "9.99", "Робуксы", "Робуксы", 2, [], true, "https://i.postimg.cc/5yZPJDmx/2845F54E-AE8D-4921-BD2C-4427AC3B4C93.jpg"],
+  [5, "1200 робуксов", "14.99", "14.99", "Робуксы", "Робуксы", 2, [], true, "https://i.postimg.cc/5yZPJDmx/2845F54E-AE8D-4921-BD2C-4427AC3B4C93.jpg"],
+  [6, "1700 робуксов", "19.99", "19.99", "Робуксы", "Робуксы", 2, [], true, "https://i.postimg.cc/5yZPJDmx/2845F54E-AE8D-4921-BD2C-4427AC3B4C93.jpg"],
+  [7, "3150 робуксов", "34.99", "34.99", "Робуксы", "Робуксы", 2, [], true, "https://i.postimg.cc/5yZPJDmx/2845F54E-AE8D-4921-BD2C-4427AC3B4C93.jpg"],
+  [7, "4500 робуксов", "49.99", "49.99", "Робуксы", "Робуксы", 2, [], true, "https://i.postimg.cc/5yZPJDmx/2845F54E-AE8D-4921-BD2C-4427AC3B4C93.jpg"],
 
 ];
 window.robloxProducts = makeProducts(robloxRaw, 'roblox_');
